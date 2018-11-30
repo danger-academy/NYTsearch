118fe3796cf3d3d8d6b445fc2a8cac5334e88fa9
MistaWizard <tylermckellar@gmail.com>
2018-11-29 21:06:22 -0500

    app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
-// var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
-// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
+var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey + "&q=";
+
+var searchTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+var articleCount = 0;
 
 $("#run-search").on("click", function(event) {
 
     event.preventDefault();
 
-var search = $("#search-term").val().trim();
-var records = $("#num-records-select").val().trim();
-var startYear = $("#start-year").val().trim();
-var endYear = $("#end-year").val().trim();
-
-var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
+// var search = $("#search-term").val().trim();
+// var records = $("#num-records-select").val().trim();
+// var startYear = $("#start-year").val().trim();
+// var endYear = $("#end-year").val().trim();
 
 $.ajax({
     url: queryURL,
