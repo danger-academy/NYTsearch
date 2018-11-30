a30f648f18918e309cb7f8129d87c55e0fa0ba06
MistaWizard <tylermckellar@gmail.com>
2018-11-29 21:26:15 -0500

    app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey + "&q=";
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey + "&q=";
 
 var searchTerm = "";
 var numResults = 0;
@@ -12,18 +12,19 @@ $("#run-search").on("click", function(event) {
 
     event.preventDefault();
 
-var searchTerm = $("#search-term").val().trim();
-console.log(searchTerm);
-var numResults = $("#num-records-select").val().trim();
+searchTerm = $("#search-term").val().trim();
+var queryURL = queryURLBase + searchTerm;
+console.log(queryURL);
+numResults = $("#num-records-select").val().trim();
 console.log(numResults);
-var startYear = $("#start-year").val().trim();
-var endYear = $("#end-year").val().trim();
+startYear = $("#start-year").val().trim();
+endYear = $("#end-year").val().trim();
 
 $.ajax({
     url: queryURL,
     method: "GET",
 }).done(function(result) {
-    console.log(queryURL);
+    // console.log(queryURL);
     console.log(result);
 }).fail(function(err) {
     throw err;
