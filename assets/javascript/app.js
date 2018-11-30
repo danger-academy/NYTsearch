0e4deb1414c77f5011c408403575b0d21b89d2bc
MistaWizard <tylermckellar@gmail.com>
2018-11-29 20:49:42 -0500

    app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,26 @@
+// var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
+
+$("#run-search").on("click", function(event) {
+
+    event.preventDefault();
+
+var search = $("#search-term").val().trim();
+var records = $("#num-records-select").val().trim();
+var startYear = $("#start-year").val().trim();
+var endYear = $("#end-year").val().trim();
+
 var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
 
-var search = $("#search-term").val();
-var records = $("#num-records-select").val();
-var startYear = $("#start-year").val();
-var endYear = $("#end-year").val();
-
 $.ajax({
-  url: queryURL,
-  method: 'GET',
+    url: queryURL,
+    method: "GET",
 }).done(function(result) {
-  console.log(result);
+    console.log(queryURL);
+    console.log(result);
 }).fail(function(err) {
-  throw err;
+    throw err;
 });
+
+})
