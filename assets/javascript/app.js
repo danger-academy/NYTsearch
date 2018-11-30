88904a5ea992850e7b4dbcb33e8ae04d560cdf42
MistaWizard <tylermckellar@gmail.com>
2018-11-29 20:17:41 -0500

    app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
 
@@ -10,6 +9,4 @@ $.ajax({
 }).fail(function(err) {
   throw err;
 });
-=======
 Trying a Push!
->>>>>>> b69352b0fd8cba6924d567c9720942b26778296d
