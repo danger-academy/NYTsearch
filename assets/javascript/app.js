c37bad5464a2bee3f9c0d4d1575e6e431574e138
o_0 <43157869+danger-academy@users.noreply.github.com>
2018-11-29 21:17:00 -0500

    tyler update to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,25 @@
 // var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
 // var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
+var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey + "&q=";
+
+var searchTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+var articleCount = 0;
 
 $("#run-search").on("click", function(event) {
 
     event.preventDefault();
 
-var search = $("#search-term").val().trim();
-var records = $("#num-records-select").val().trim();
+var searchTerm = $("#search-term").val().trim();
+console.log(searchTerm);
+var numResults = $("#num-records-select").val().trim();
+console.log(numResults);
 var startYear = $("#start-year").val().trim();
 var endYear = $("#end-year").val().trim();
-
-var APIKey = "0aae7f54e5404ddaa31097eea03cd23e";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + APIKey;
 
 $.ajax({
     url: queryURL,
